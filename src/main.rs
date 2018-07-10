e3d0233e46c303fe414aeb92e80ba9db8e162645
Guillaume Charmetant <cguille.dev@gmail.com>
2018-07-10 13:26:18 +0200

    add dep to actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate actix_web;
+use actix_web::{server, App, HttpRequest};
+
+fn index(_req: HttpRequest) -> &'static str {
+    "Hello :-)"
+}
+
 fn main() {
-    println!("Hello, world!");
+    let binding = "127.0.0.1:8088";
+
+    server::new(|| App::new().resource("/", |r| r.f(index)))
+        .bind(binding)
+        .unwrap()
+        .run();
 }
