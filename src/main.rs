58af210f3eeb49dbb23d567b723ae5834daecfb9
Guillaume Charmetant <cguille.dev@gmail.com>
2018-09-14 17:47:49 +0200

    allow binding to be configured through env var

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 extern crate actix_web;
 use actix_web::{server, App, HttpRequest};
 
+use std::env;
+
 fn index(_req: HttpRequest) -> &'static str {
     "Hello :-)"
 }
 
 fn main() {
-    let binding = "127.0.0.1:8088";
+    let binding = match env::var("OPOSSYNC_BIND") {
+        Ok(value) => value,
+        Err(err_kind) => match err_kind {
+            env::VarError::NotPresent => "127.0.0.1:8088".to_string(),
+            env::VarError::NotUnicode(value) => panic!("Invalid bind value: {:?}", value),
+        }
+    };
 
     server::new(|| App::new().resource("/", |r| r.f(index)))
         .bind(binding)
