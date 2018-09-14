8fb06dbc29c54649c3b450d723abcd29aa4e81f2
Guillaume Charmetant <cguille.dev@gmail.com>
2018-09-14 17:48:23 +0200

    add logging

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,29 @@ use actix_web::{server, App, HttpRequest};
 
 use std::env;
 
+#[macro_use] extern crate log;
+extern crate simplelog;
+use simplelog::*;
+
 fn index(_req: HttpRequest) -> &'static str {
     "Hello :-)"
 }
 
 fn main() {
+    SimpleLogger::init(LevelFilter::Info, Config::default()).expect("Set up logger");
+
+    info!("Looking up binding from env var 'OPOSSYNC_BIND'");
+
     let binding = match env::var("OPOSSYNC_BIND") {
-        Ok(value) => value,
+        Ok(value) => {
+            info!("Found binding '{}'", value);
+            value
+        },
         Err(err_kind) => match err_kind {
-            env::VarError::NotPresent => "127.0.0.1:8088".to_string(),
+            env::VarError::NotPresent => {
+                info!("Falling back to default binding");
+                "127.0.0.1:8088".to_string()
+            },
             env::VarError::NotUnicode(value) => panic!("Invalid bind value: {:?}", value),
         }
     };
